32d15da309b0db1e4c81aa4739d25fcf0a73cb2e
TestSrinSS01 <srinjoysinha1@gmail.com>
2020-11-29 01:03:06 +0530

    Bug fixed

--- src/main/java/me/srinjoy/Main.java
+++ src/main/java/me/srinjoy/Main.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -46,6 +47,7 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("logo.png")));
         primaryStage.setOnCloseRequest(event -> {
             lib.Discord_Shutdown();
             System.exit(0);
---
+++ src/main/java/me/srinjoy/Start.java
@@ -0,0 +1,7 @@
+package me.srinjoy;
+
+public class Start {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
